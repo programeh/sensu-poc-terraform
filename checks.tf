9aec808aa13bbe8c836122939946050638c500d9
Asutosh Ghanto <asutosh@clevertap.com>
2024-10-23 16:23:48 +0530

    my second commit

--- checks.tf
+++ checks.tf
@@ -1,6 +1,6 @@
 resource "sensu_check" "billing-availability" {
   namespace   = "default"
-  command        = "check_http -u /health -H 127.0.0.1 -p 80"
+  command        = "check_http -u / -H 127.0.0.1 -p 80"
   name           = "nginx-availability"
   subscriptions  = ["nginx"]
   runtime_assets = ["monitoring-plugins"]
