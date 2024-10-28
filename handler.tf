f8f7adf124a7b194b14a2d6c66875daef4a20349
Asutosh Ghanto <asutosh@clevertap.com>
2024-10-29 00:35:27 +0530

    my second commit

--- handler.tf
+++ handler.tf
@@ -81,51 +81,56 @@ resource "sensu_handler" "SensuRemediationHandler" {
   command = "sensu-go-remediation-handler"
   namespace = "default"
   timeout = 10
-  runtime_assets = [sensu_asset.remediation_handler_asset.name]
-  env_vars = {
-    SENSU_API_USER="pavan"
-    SENSU_API_PASS="password"
-    SENSU_API_URL="http://43.203.207.128:8080"
-  }
-}
-
-resource "sensu_handler" "SensuCustomHandler" {
-  name = "remediation-handler-v2"
-  type = "pipe"
-  command = "execute-runcommand"
-  namespace = "default"
-  timeout = 10
-  runtime_assets = [sensu_asset.sensu_go_assetsv2_default.name]
-  filters = ["is_incident","one_min_delay_fatigue_check","fatigue_check"]
-}
-
-resource "sensu_handler" "pagerdutyV3" {
-  name      = "pagerdutyV3"
-  namespace = "default"
-  type      = "pipe"
-  command   = "sensu-pagerduty-handler  --details-format json"
-  timeout   = 0
-  env_vars  = {
-    PAGERDUTY_TOKEN = "28b3ba1e09714e07c0c610ab02c2c29b"
-  }
   filters = [
     "is_incident",
     "not_silenced",
     "fatigue_check",
-    "dependencies",
-    "five_min_delay_fatigue_check"
+    "dependencies"
   ]
-  runtime_assets = [
-    sensu_asset.sensuPagerdutyHandler.name
-  ]
+  runtime_assets = [sensu_asset.remediation_handler_asset.name]
+  env_vars = {
+    SENSU_API_USER="remediation-handler"
+    SENSU_API_PASS="REPLACEME"
+    SENSU_API_URL="http://43.203.207.128:8080"
+  }
 }
+
+#resource "sensu_handler" "SensuCustomHandler" {
+#  name = "remediation-handler-v2"
+#  type = "pipe"
+#  command = "execute-runcommand"
+#  namespace = "default"
+#  timeout = 10
+#  runtime_assets = [sensu_asset.sensu_go_assetsv2_default.name]
+#  filters = ["is_incident","one_min_delay_fatigue_check","fatigue_check"]
+#}
+
+#resource "sensu_handler" "pagerdutyV3" {
+#  name      = "pagerdutyV3"
+#  namespace = "default"
+#  type      = "pipe"
+#  command   = "sensu-pagerduty-handler  --details-format json"
+#  timeout   = 0
+#  env_vars  = {
+#    PAGERDUTY_TOKEN = "28b3ba1e09714e07c0c610ab02c2c29b"
+#  }
+#  filters = [
+#    "is_incident",
+#    "not_silenced",
+#    "fatigue_check",
+#    "dependencies",
+#    "five_min_delay_fatigue_check"
+#  ]
+#  runtime_assets = [
+#    sensu_asset.sensuPagerdutyHandler.name
+#  ]
+#}
 
 resource "sensu_handler" "alert_stack" {
   name = "alert_stack"
   type = "set"
   handlers = [
-    "pagerdutyWarningHandler",
-    "remediation-handler-v2",
-    "pagerdutyV3"
+    "pagerdutyV2",
+    "remediation",
   ]
 }
